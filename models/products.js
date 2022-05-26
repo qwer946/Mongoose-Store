7cc3b4500d20ff923fd425dc0b6cb5d22f2b3899
qwer946 <khalid.ali.987@gmail.com>
2022-05-25 21:37:35 -0500

    last thing' '

--- models/products.js
+++ models/products.js
@@ -4,14 +4,14 @@
 const mongoose = require("mongoose");
 
 // we are creating a new book schema object.
-const productSchema = new mongoose.Schema({
-  name: { type: "string", required: true },
-  description: { type: "string", required: true },
-  img: { type: "string", required: true },
-  price: { type: "string", required: true },
-  qty: { type: "string", required: true },
+const bookSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: String,
+  img: String,
+  price: Number,
+  qty: Number,
 });
 
-const Product = mongoose.model("Product", productSchema);
+const Book = mongoose.model("Book", bookSchema);
 
-module.exports = Product;
+module.exports = Book;
--- server.js
+++ server.js
@@ -2,16 +2,17 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-// need to download npm mongoose step 2
 const mongoose = require("mongoose");
-// after we complete our schema we plug this in
 const Book = require("./models/products");
-// Database Connection step 2
+const methodOverride = require("method-override");
+// const Books = require("./models/Seed");
+
+// Database Connection
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-// step 2 to make sure your database is connected
+
 // Database Connection Error/Success
 // Define callback functions for various events
 const db = mongoose.connection;
@@ -19,41 +20,22 @@ db.on("error", (err) => console.log(err.message + " is mongo not running?"));
 db.on("connected", () => console.log("mongo connected"));
 db.on("disconnected", () => console.log("mongo disconnected"));
 
-// Middleware
-// Body parser middleware: give us access to req.body
-// step 3
+// MIDDLEWARE & BODY PARSER
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
-// Create
-// step 3 we are creating a route to say "recieved"
-app.post("/books", (req, res) => {
-  if (req.body.completed === "on") {
-    //if checked, req.body.completed is set to 'on'
-    req.body.completed = true;
-  } else {
-    //if not checked, req.body.completed is undefined
-    req.body.completed = false;
-  }
-  // it is a function in mongoose, so we create Book
-  Book.create(req.body, (error, createdBook) => {
+// SEED DATA!
+const bookSeed = require("./models/Seed");
+app.get("/books/seed", (req, res) => {
+  Book.deleteMany({}, (error, allBooks) => {});
+
+  Book.create(bookSeed, (error, data) => {
     res.redirect("/books");
   });
-
-  //   //   res.send(req.body);
 });
 
-// Routes / Controllers
-
-// step 4 create a new route
-// New
-// create a new route
-app.get("/books/new", (req, res) => {
-  res.render("new.ejs");
-});
-
-// step 5 make a forEach loop in the index.ejs
-// Index
-// Creating a index route
+// ROUTES
+// INDEX
 app.get("/books", (req, res) => {
   Book.find({}, (error, allBooks) => {
     res.render("index.ejs", {
@@ -61,13 +43,64 @@ app.get("/books", (req, res) => {
     });
   });
 });
-// Seed DATA
 
-// Routes / Controllers
+// NEW
+app.get("/books/new", (req, res) => {
+  res.render("new.ejs");
+});
 
-// Seed
+// DELETE
+app.delete("/books/:id", (req, res) => {
+  Book.findByIdAndDelete(req.params.id, (err, data) => {
+    res.redirect("/books");
+  });
+});
 
-// Show
+// UPDATE
+app.put("/books/:id", (req, res) => {
+  if (req.body.completed === "on") {
+    req.body.completed = true;
+  } else {
+    req.body.completed = false;
+  }
+
+  Book.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    },
+    (error, updatedBook) => {
+      res.redirect(`/books/${req.params.id}`);
+    }
+  );
+});
+
+// CREATE
+// app.post("/books", (req, res) => {
+//   if (req.body.completed === "on") {
+//     //if checked, req.body.completed is set to 'on'
+//     req.body.completed = true;
+//   } else {
+//     //if not checked, req.body.completed is undefined
+//     req.body.completed = false;
+//   }
+
+//   Book.create(req.body, (error, createdBook) => {
+//     res.redirect("/books");
+//   });
+// });
+
+// EDIT
+app.get("/books/:id/edit", (req, res) => {
+  Book.findById(req.params.id, (error, foundBook) => {
+    res.render("edit.ejs", {
+      book: foundBook,
+    });
+  });
+});
+
+// SHOW
 app.get("/books/:id", (req, res) => {
   Book.findById(req.params.id, (err, foundBook) => {
     res.render("show.ejs", {
@@ -75,6 +108,9 @@ app.get("/books/:id", (req, res) => {
     });
   });
 });
-// Listener
+
+// Listenter
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`server listening on port: ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`The server is listening on port: ${PORT}`);
+});
